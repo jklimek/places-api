fd8a8e940ed45b272082a8e4f7e485fcb6365d5f
Jakub Klimek <jklimek@fidolabs.com>
2016-08-10 03:23:13 +0200

    Fron page styling Added functional navbar v0.1 Testing geolocation v0.1

--- src/AppBundle/Controller/MainController.php
+++ src/AppBundle/Controller/MainController.php
@@ -6,6 +6,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use GuzzleHttp;
+use Symfony\Component\HttpFoundation\Request;
 
 class MainController extends Controller
 {
@@ -13,17 +14,24 @@ class MainController extends Controller
     /**
      * @Route("/", name="homepage")
      * @Template
+     * @param Request $request
      * @return array
+     * @throws \Exception
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
+        $parameters = [
+            "type" => $request->get('type'),
+            "location" => $request->get('location'),
+        ];
+
 
         $url = "http://places.klemens.ninja/places";
 //        $url = "http://0.0.0.0:8888/places";
         $options = [
             "location" => "54.348538,18.653228", // Default location - Neptune's Fountain
             "radius" => 2000,
-            "type" => "bar", // Default type - bar
+            "type" => $parameters["type"], // Default type - bar
         ];
         $responseBody = $this->get('api.requests.service')->makeJsonRequest($url, $options);
         return ["response" => $responseBody];
--- src/AppBundle/Resources/web/js/custom.js
+++ src/AppBundle/Resources/web/js/custom.js
@@ -1,3 +1,40 @@
 /**
  * Created by klemens on 07/08/16.
  */
+
+
+$("#geoLocationButton").click(function () {
+    $("#geoLocationIcon").removeClass("fa-location-arrow");
+    $("#geoLocationIcon").addClass("fa-spinner fa-pulse");
+    getLocation();
+});
+
+function getLocation() {
+    if (navigator.geolocation) {
+        var timeoutVal = 10 * 1000 * 1000;
+        navigator.geolocation.getCurrentPosition(
+            displayPosition,
+            displayError,
+            {enableHighAccuracy: true, timeout: timeoutVal, maximumAge: 0}
+        );
+    }
+    else {
+        alert("Geolocation is not supported by this browser");
+    }
+}
+
+function displayPosition(position) {
+    $("#geoLocationIcon").addClass("fa-location-arrow");
+    $("#geoLocationIcon").removeClass("fa-spinner fa-pulse");
+    $("#locationTest").text("Latitude: " + position.coords.latitude + ", Longitude: " + position.coords.longitude);
+}
+function displayError(error) {
+    $("#geoLocationIcon").addClass("fa-location-arrow");
+    $("#geoLocationIcon").removeClass("fa-spinner fa-pulse");
+    var errors = {
+        1: 'Permission denied',
+        2: 'Position unavailable',
+        3: 'Request timeout'
+    };
+    $("#locationTest").text("Error: " + errors[error.code]);
+}
